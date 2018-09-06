9ad6fd470510a8c385ecae2350209a40fb6753e2
Москалев Роман <moslik123@gmail.com>
2018-09-06 18:32:08 +0700

    Дополнена форма MainForm полями просмотра контакта и полем find

--- ContactsAppUI/ContactsAppUI/MainForm.Designer.cs
+++ ContactsAppUI/ContactsAppUI/MainForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -38,8 +39,27 @@
             this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.BirthdayDayTool = new System.Windows.Forms.DateTimePicker();
+            this.VkTextbox = new System.Windows.Forms.TextBox();
+            this.EmailTextbox = new System.Windows.Forms.TextBox();
+            this.PhoneTextbox = new System.Windows.Forms.TextBox();
+            this.NameTextbox = new System.Windows.Forms.TextBox();
+            this.SubnameTextbox = new System.Windows.Forms.TextBox();
+            this.VkLabel = new System.Windows.Forms.Label();
+            this.EmailLabel = new System.Windows.Forms.Label();
+            this.PhoneLabel = new System.Windows.Forms.Label();
+            this.BirthdayLabel = new System.Windows.Forms.Label();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.SubnameLabebl = new System.Windows.Forms.Label();
+            this.FindLabel = new System.Windows.Forms.Label();
+            this.FindTextbox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -90,13 +110,13 @@
             // editContactToolStripMenuItem
             // 
             this.editContactToolStripMenuItem.Name = "editContactToolStripMenuItem";
-            this.editContactToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.editContactToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.editContactToolStripMenuItem.Text = "Edit Contact";
             // 
             // removeContactToolStripMenuItem
             // 
             this.removeContactToolStripMenuItem.Name = "removeContactToolStripMenuItem";
-            this.removeContactToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.removeContactToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.removeContactToolStripMenuItem.Text = "Remove Contact";
             // 
             // helpToolStripMenuItem
@@ -110,7 +130,7 @@
             // aboutToolStripMenuItem
             // 
             this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
-            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.aboutToolStripMenuItem.Text = "About";
             this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
             // 
@@ -119,9 +139,193 @@
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer1.Location = new System.Drawing.Point(0, 24);
             this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.splitContainer1.Panel1.Controls.Add(this.button3);
+            this.splitContainer1.Panel1.Controls.Add(this.button2);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Controls.Add(this.FindTextbox);
+            this.splitContainer1.Panel1.Controls.Add(this.FindLabel);
+            this.splitContainer1.Panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.splitContainer1.Panel2.Controls.Add(this.BirthdayDayTool);
+            this.splitContainer1.Panel2.Controls.Add(this.VkTextbox);
+            this.splitContainer1.Panel2.Controls.Add(this.EmailTextbox);
+            this.splitContainer1.Panel2.Controls.Add(this.PhoneTextbox);
+            this.splitContainer1.Panel2.Controls.Add(this.NameTextbox);
+            this.splitContainer1.Panel2.Controls.Add(this.SubnameTextbox);
+            this.splitContainer1.Panel2.Controls.Add(this.VkLabel);
+            this.splitContainer1.Panel2.Controls.Add(this.EmailLabel);
+            this.splitContainer1.Panel2.Controls.Add(this.PhoneLabel);
+            this.splitContainer1.Panel2.Controls.Add(this.BirthdayLabel);
+            this.splitContainer1.Panel2.Controls.Add(this.NameLabel);
+            this.splitContainer1.Panel2.Controls.Add(this.SubnameLabebl);
+            this.splitContainer1.Panel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.splitContainer1.Size = new System.Drawing.Size(967, 534);
-            this.splitContainer1.SplitterDistance = 368;
+            this.splitContainer1.SplitterDistance = 393;
             this.splitContainer1.TabIndex = 1;
+            // 
+            // BirthdayDayTool
+            // 
+            this.BirthdayDayTool.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BirthdayDayTool.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.BirthdayDayTool.Location = new System.Drawing.Point(84, 60);
+            this.BirthdayDayTool.MaxDate = new System.DateTime(2018, 9, 6, 0, 0, 0, 0);
+            this.BirthdayDayTool.MinDate = new System.DateTime(1900, 1, 1, 0, 0, 0, 0);
+            this.BirthdayDayTool.Name = "BirthdayDayTool";
+            this.BirthdayDayTool.Size = new System.Drawing.Size(115, 22);
+            this.BirthdayDayTool.TabIndex = 14;
+            this.BirthdayDayTool.Value = new System.DateTime(2000, 1, 1, 0, 0, 0, 0);
+            // 
+            // VkTextbox
+            // 
+            this.VkTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.VkTextbox.Location = new System.Drawing.Point(84, 144);
+            this.VkTextbox.Name = "VkTextbox";
+            this.VkTextbox.Size = new System.Drawing.Size(473, 22);
+            this.VkTextbox.TabIndex = 9;
+            // 
+            // EmailTextbox
+            // 
+            this.EmailTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.EmailTextbox.Location = new System.Drawing.Point(84, 116);
+            this.EmailTextbox.Name = "EmailTextbox";
+            this.EmailTextbox.Size = new System.Drawing.Size(473, 22);
+            this.EmailTextbox.TabIndex = 10;
+            // 
+            // PhoneTextbox
+            // 
+            this.PhoneTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.PhoneTextbox.Location = new System.Drawing.Point(84, 88);
+            this.PhoneTextbox.Name = "PhoneTextbox";
+            this.PhoneTextbox.Size = new System.Drawing.Size(473, 22);
+            this.PhoneTextbox.TabIndex = 11;
+            // 
+            // NameTextbox
+            // 
+            this.NameTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.NameTextbox.Location = new System.Drawing.Point(84, 32);
+            this.NameTextbox.Name = "NameTextbox";
+            this.NameTextbox.Size = new System.Drawing.Size(473, 22);
+            this.NameTextbox.TabIndex = 12;
+            // 
+            // SubnameTextbox
+            // 
+            this.SubnameTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SubnameTextbox.Location = new System.Drawing.Point(84, 4);
+            this.SubnameTextbox.Name = "SubnameTextbox";
+            this.SubnameTextbox.Size = new System.Drawing.Size(473, 22);
+            this.SubnameTextbox.TabIndex = 13;
+            // 
+            // VkLabel
+            // 
+            this.VkLabel.AutoSize = true;
+            this.VkLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.VkLabel.Location = new System.Drawing.Point(24, 147);
+            this.VkLabel.Name = "VkLabel";
+            this.VkLabel.Size = new System.Drawing.Size(54, 16);
+            this.VkLabel.TabIndex = 3;
+            this.VkLabel.Text = "vk.com:";
+            // 
+            // EmailLabel
+            // 
+            this.EmailLabel.AutoSize = true;
+            this.EmailLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.EmailLabel.Location = new System.Drawing.Point(29, 119);
+            this.EmailLabel.Name = "EmailLabel";
+            this.EmailLabel.Size = new System.Drawing.Size(49, 16);
+            this.EmailLabel.TabIndex = 4;
+            this.EmailLabel.Text = "E-mail:";
+            // 
+            // PhoneLabel
+            // 
+            this.PhoneLabel.AutoSize = true;
+            this.PhoneLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.PhoneLabel.Location = new System.Drawing.Point(28, 91);
+            this.PhoneLabel.Name = "PhoneLabel";
+            this.PhoneLabel.Size = new System.Drawing.Size(50, 16);
+            this.PhoneLabel.TabIndex = 5;
+            this.PhoneLabel.Text = "Phone:";
+            // 
+            // BirthdayLabel
+            // 
+            this.BirthdayLabel.AutoSize = true;
+            this.BirthdayLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BirthdayLabel.Location = new System.Drawing.Point(18, 65);
+            this.BirthdayLabel.Name = "BirthdayLabel";
+            this.BirthdayLabel.Size = new System.Drawing.Size(60, 16);
+            this.BirthdayLabel.TabIndex = 6;
+            this.BirthdayLabel.Text = "Birthday:";
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.NameLabel.Location = new System.Drawing.Point(30, 35);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(48, 16);
+            this.NameLabel.TabIndex = 7;
+            this.NameLabel.Text = "Name:";
+            // 
+            // SubnameLabebl
+            // 
+            this.SubnameLabebl.AutoSize = true;
+            this.SubnameLabebl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SubnameLabebl.Location = new System.Drawing.Point(9, 7);
+            this.SubnameLabebl.Name = "SubnameLabebl";
+            this.SubnameLabebl.Size = new System.Drawing.Size(69, 16);
+            this.SubnameLabebl.TabIndex = 8;
+            this.SubnameLabebl.Text = "Subname:";
+            // 
+            // FindLabel
+            // 
+            this.FindLabel.AutoSize = true;
+            this.FindLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FindLabel.Location = new System.Drawing.Point(12, 10);
+            this.FindLabel.Name = "FindLabel";
+            this.FindLabel.Size = new System.Drawing.Size(37, 16);
+            this.FindLabel.TabIndex = 0;
+            this.FindLabel.Text = "Find:";
+            // 
+            // FindTextbox
+            // 
+            this.FindTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FindTextbox.Location = new System.Drawing.Point(55, 10);
+            this.FindTextbox.Name = "FindTextbox";
+            this.FindTextbox.Size = new System.Drawing.Size(326, 22);
+            this.FindTextbox.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 499);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 499);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Edit";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(174, 499);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Remove";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
@@ -130,11 +334,16 @@
             this.ClientSize = new System.Drawing.Size(967, 558);
             this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "MainForm";
             this.Text = "ContactsApp";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
@@ -154,6 +363,23 @@
         private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
         private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox FindTextbox;
+        private System.Windows.Forms.Label FindLabel;
+        private System.Windows.Forms.DateTimePicker BirthdayDayTool;
+        private System.Windows.Forms.TextBox VkTextbox;
+        private System.Windows.Forms.TextBox EmailTextbox;
+        private System.Windows.Forms.TextBox PhoneTextbox;
+        private System.Windows.Forms.TextBox NameTextbox;
+        private System.Windows.Forms.TextBox SubnameTextbox;
+        private System.Windows.Forms.Label VkLabel;
+        private System.Windows.Forms.Label EmailLabel;
+        private System.Windows.Forms.Label PhoneLabel;
+        private System.Windows.Forms.Label BirthdayLabel;
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.Label SubnameLabebl;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
